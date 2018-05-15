8d628ca7fb7eb2d73588b9cc2ef4b6bb25c825fe
TakakiNishio <p104314t@mail.kyutech.jp>
2018-05-15 20:37:57 +0900

    [delete] all scripts without dataset_processing

--- YOLOv2/person.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python yolov2_prediction_find_person.py -v ../../dataset/videos/tsukuba/0923/camera_0.avi -s 56
-python yolov2_prediction_find_person.py -v ../../dataset/videos/tsukuba/0923/camera_1.avi -s 57
-python yolov2_prediction_find_person.py -v ../../dataset/videos/tsukuba/0923/camera_2.avi -s 58
